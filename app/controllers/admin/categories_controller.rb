bfa1bb16c8949b30c2d63dc1f1b4599076d9b4ad
bfetler <bfetler@gmail.com>
2013-03-23 17:49:52 -0700

    clean up comments

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -24,20 +24,14 @@ class Admin::CategoriesController < Admin::BaseController
   private
 
   def new_or_edit
-    puts "new_or_edit params: " + params.inspect
+    # puts "new_or_edit params: " + params.inspect
     @categories = Category.find(:all)
     # @category = Category.find(params[:id])  # orig
-    #if params[:id]
-    #  @category = Category.find(params[:id])
-    #else
-    #  @category = Category.new
-    #end
     @category = params[:id] ? Category.find(params[:id]) : Category.new
     @category.attributes = params[:category]
     if request.post?
       respond_to do |format|
         format.html do
-          puts "new_or_edit calling save_category"
           save_category
         end
         format.js do 
@@ -53,9 +47,7 @@ class Admin::CategoriesController < Admin::BaseController
   end
 
   def save_category
-    #puts "save_category params: " + params.inspect
-    #puts "@category: " + @category.inspect
-#    if @category.save!  # orig
+    # if @category.save!  # orig
     if @category.save
       flash[:notice] = _('Category was successfully saved.')
     else
